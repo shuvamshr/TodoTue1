ad2260b7be14d97cb02936aa82124d86a4d8ded2
shuvamshr <shuvamshr@outlook.com>
2026-03-24 13:54:22 +1100

    add: form and enum

--- TodoTue1/ContentView.swift
+++ TodoTue1/ContentView.swift
@@ -14,6 +14,16 @@ struct TodoTask: Identifiable {
     var title: String = "Untitled"
     var description: String = "No Description Provided"
     var isCompleted: Bool = false
+    var priority: Priority = .medium
+   
+}
+
+
+enum Priority: String, CaseIterable {
+    case low = "Low Priority"
+    case medium = "Medium Priority"
+    case high = "High Priority"
+    case urgent = "Urgent Priority"
 }
 
 
@@ -21,45 +31,116 @@ struct RootView: View {
     
     // var tasks: [String] = ["Do this", "Do that", "And that"]
     
-    var tasks: [TodoTask] = [
+    @State private var tasks: [TodoTask] = [
         TodoTask(title: "My First Task is something", description: "Some description about this task"),
         TodoTask(title: "My Second Task", description: "Some description about this task"),
         TodoTask(title: "My Third Task", description: "Some description about this task", isCompleted: true),
+        TodoTask()
     ]
     
     var body: some View {
         
         // for task in tasks { task.title, task.description, task.isCompleted }
-        ScrollView {
-            ForEach(tasks) { task in
-                TaskItemView(title: task.title, description: task.description, isCompleted: task.isCompleted)
+        NavigationStack {
+            ScrollView {
+                ForEach(tasks) { task in
+                    TaskItemView(task: task)
+                }
+                
+            
+            }
+            .navigationTitle("Things To-Do")
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink {
+                        NewTaskView(tasks: $tasks)
+                    } label: {
+                        Label("Add New Task", systemImage: "plus")
+                    }
+                    .buttonStyle(.borderedProminent)
+                }
             }
         }
+    }
+
+}
+
+struct NewTaskView: View {
+    
+    @Binding var tasks: [TodoTask]
+    
+    @State private var title: String = ""
+    @State private var description: String = ""
+    @State private var priority: Priority = .medium
+    
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        Form {
+            TextField("Enter Your Task Title", text: $title)
+            TextEditor(text: $description)
+            Picker("Select Prioirity", selection: $priority) {
+                ForEach(Priority.allCases, id: \.self) { priority in
+                    Text(priority.rawValue)
+                        .tag(priority)
+                }
+            }
+        }
+        .navigationTitle("New Task Form")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    addNewTask()
+                    dismiss()
+                } label: {
+                    Label("Save", systemImage: "checkmark")
+                }
+                .buttonStyle(.borderedProminent)
+                .disabled(!isValidForm)
+            }
+        }
+    }
+    
+    private func addNewTask() {
+        let newTask = TodoTask(title: title, description: description.isEmpty ? "No Description" : description, priority: priority)
         
+        tasks.append(newTask)
+    }
+    
+    private var isValidForm: Bool {
+        if title.isEmpty {
+            return false
+        } else {
+            return true
+        }
     }
 }
 
 struct TaskItemView: View {
     
-    var title: String
-    var description: String
-    var isCompleted: Bool
+    var task: TodoTask
     
     var body: some View {
         
         // (V)ertically, (H)orizontal, (Z)-Index
         HStack(alignment: .top) {
             VStack(alignment: .leading, spacing: 4) {
-                Text(self.title)
+                Button(task.priority.rawValue) {
+                    
+                }
+                .controlSize(.mini)
+                .buttonStyle(.borderedProminent)
+                Text(task.title)
                     .font(.headline)
                     .foregroundStyle(Color.primary)
-                Text(self.description)
+                Text(task.description)
                     .font(.subheadline)
                     .foregroundStyle(Color.secondary)
             }
             Spacer()
             
-            if self.isCompleted {
+            if task.isCompleted {
                 
                 Image(systemName: "checkmark.circle.fill")
                     .font(.title)
